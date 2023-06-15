2ac45c6202aefb963afddae22a22300acf0c36b4
Melissa Dufrechou <dufrechou82@gmail.com>
2023-06-14 20:48:42 -0500

    Some starting logic. changePlayer function not working properly

--- scripts/index.js
+++ scripts/index.js
@@ -44,25 +44,76 @@ const Players = {
 }
 let currentPlayer = Players.player1
 
-console.log(displayPlayer)
 console.log(currentPlayer)
 
-if (currentPlayer = Players.player1){
-    displayPlayer.textContent = "Player One"
-} else {
-    displayPlayer.textContent = "Player Two"
-}
+const changePlayers = () => {
+    if (currentPlayer = Players.player1){
+        currentPlayer = Players.player2
+        displayPlayer.textContent = "Player Two";
+    }else if (currentPlayer = Players.player2)
+        {currentPlayer = Player.player1
+        displayPlayer.textContent = "Player One";
+    }}
+
+
+changePlayers()
+
+console.log(currentPlayer)
+
+changePlayers();
+
+console.log(currentPlayer);
+
+// if (currentPlayer = Players.player1){
+//     displayPlayer.textContent = "Player One"
+// } else {
+//     displayPlayer.textContent = "Player Two"
+// }
+
 
 //***************Begin Play*******************/
 
-let question = document.getElementsByClassName("question")
+let card = document.getElementsByClassName("card")
+let input = document.getElementById("user-input").value
+let cardSelected = false
+// let answer
 
-for (let i = 0; i < question.length; i++){
-    question[i].addEventListener("click", (e) => {
-        question[i].textContent = placeholderQuestions[i].question
-        console.log(`Question ${i} was clicked`)
-        guessBtn.disabled=false
-        passBtn.disabled=false
-        roundTwoBtn.disabled=false
+
+for (let i = 0; i < card.length; i++){
+    
+    card[i].addEventListener("click", (e) => {
+        //captures point value of selected question and parses to number
+        let possiblePoints = Number(card[i].innerHTML)
+        console.log(possiblePoints)
+        if (!cardSelected){ // set to false will allow user to select a card
+          
+            //will loop through each question in relationships to the card selected
+        card[i].textContent = placeholderQuestions[i].question;
+        console.log(`Question ${i} was clicked`);
+          
+          //new cards can't be selected until either guess or pass buttons are clicked. They can now enabled
+        cardSelected = true;
+        guessBtn.disabled = false;
+        passBtn.disabled = false;
+          
+          //will loop through each answer and be the same as the corresponding card
+        guessBtn.addEventListener("click", e =>{
+        if(placeholderQuestions[i].answer === input){
+            currentPlayer.points += possiblePoints
+            console.log(currentPlayer.points)
+        }})
+          
+        }
     })
 }
+    
+
+
+// for (let i = 0; i < placeholderQuestions.length; i++){
+// guessBtn.addEventListener("click", e =>{
+//     console.log(placeholderQuestions[i].answer)
+//     console.log(input)
+//     console.log(placeholderQuestions[i].answer === input)
+//     cardSelected = false
+// })
+// }
