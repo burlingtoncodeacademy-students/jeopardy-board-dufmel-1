9185cb16a71302a20cae1f81e9b5824fbb559b8a
Melissa Dufrechou <dufrechou82@gmail.com>
2023-06-16 21:55:10 -0500

    Too tired to keep going

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@
 import placeholderQuestions from "./placeholder-questions.js";
 console.log({ placeholderQuestions });
 
+
+
+
 //****************ROUND ONE JS****************************/
 
 //Button Set-up
@@ -17,11 +20,11 @@ passBtn.disabled = false
 let displayPlayer = document.getElementById("display-player")
 
 class Player {
-constructor(player, points){
+    constructor(player, points){
         this.player = player,
         this.points = points
     }
-
+    
 }
 
 //Dictionary of Players will give easy access to players and their properties
@@ -29,68 +32,72 @@ const Players = {
     player1: new Player(
         "player1",
         0
-    ),
-
-    player2: new Player(
-        "player2",
-        0
-    ),
-
-    changePlayers (p) {
-        if (p === this.player1){
-            currentPlayer = this.player2;
-            displayPlayer.textContent = "Player Two";
+        ),
+        
+        player2: new Player(
+            "player2",
+            0
+            ),
             
-        }else if (p === this.player2)
-            {currentPlayer = this.player1;
-            displayPlayer.textContent = "Player One";
+            changePlayers (p) {
+                if (p === this.player1){
+                    currentPlayer = this.player2;
+                    displayPlayer.textContent = "Player Two";
+                    
+                }else if (p === this.player2)
+                {currentPlayer = this.player1;
+                    displayPlayer.textContent = "Player One";
+                }
+            },
+            
+            addPoints(points){
+                return currentPlayer.points += points
+            },
+            
+            subtractPoints(points){
+                return currentPlayer.points -= points
+            }
         }
-    },
-
-    addPoints(points){
-        return currentPlayer.points += points
-    }
-}
-let currentPlayer = Players.player1
-
-// console.log(Players.addPoints(200))
-
-// console.log(currentPlayer.player)
-
-passBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-  if (!passBtn.disabled) {
-      console.log(currentPlayer)
-    Players.changePlayers(currentPlayer);
-      console.log(currentPlayer);
-  }
-});
-
-if (currentPlayer = Players.player1){
-    displayPlayer.textContent = "Player One"
-} else {
-    displayPlayer.textContent = "Player Two"
-}
-
-
-//***************Begin Play*******************/
-
-let category = document.getElementsByClassName("category-header")
-let natureCategory = document.getElementById("nature-category")
-let animalCategory = document.getElementById("animal-category")
-let nature = document.getElementsByClassName("nature")
-let animal = document.getElementsByClassName("animal")
-let card = document.getElementsByClassName("card")
-let input = document.getElementById("user-input")
-let modal = document.getElementById("myModal");
-let cardSelected = false
-
-//Add questions from placeholder questions to array by category
-
+        let currentPlayer = Players.player1
+        
+        // console.log(Players.addPoints(200))
+        
+        // console.log(currentPlayer.player)
+        
+        passBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            if (!passBtn.disabled) {
+                console.log(currentPlayer)
+                Players.changePlayers(currentPlayer);
+                console.log(currentPlayer);
+            }
+        });
+        
+        if (currentPlayer = Players.player1){
+            displayPlayer.textContent = "Player One"
+        } else {
+            displayPlayer.textContent = "Player Two"
+        }
+        
+        
+        //***************Begin Play*******************/
+        
+        let category = document.getElementsByClassName("category-header")
+        let animalCategory = document.getElementById("animal-category")
+        let natureCategory = document.getElementById("nature-category")
+        let nature = document.getElementsByClassName("nature")
+        let animal = document.getElementsByClassName("animal")
+        let card = document.getElementsByClassName("card")
+        let input = document.getElementById("user-input")
+        let modal = document.getElementById("myModal");
+        let cardSelected = false
+        
+        //Add questions from placeholder questions to array by category
+        
 let natureArray=placeholderQuestions.filter((obj) => obj.category === "Nature")
 let animalArray = placeholderQuestions.filter((obj) => obj.category === "Animals")
-let categorySelection
-console.log(animalArray)
+
+
 
 //**************************************www.toptal.com/javascript/10-most-common-javascript-mistakes*****************************************/
 let answer = []
@@ -100,6 +107,35 @@ let possiblePoints = []
 for (let i = 0; i < category.lenth; i++){
     category.textContent = placeholderQuestions.category.value
 }
+
+//REMOVE RANDOM ITEM FROM ARRAY
+
+card.onclick = flipOver()
+
+function flipOver(){
+    answer.length > 0 ? answer.pop() : null
+    possiblePoints.length > 0 ? possiblePoints.pop() : null
+    if (!cardSelected){ // set to false will allow user to select a card
+    const random = Math.floor(Math.random() * natureArray.length)
+    const newArray = natureArray.splice(random, 1)[0]
+    console.log(newArray)
+    
+    //captures point value of selected question and parses to number
+    possiblePoints.push(nature[num].innerHTML)
+
+            //will loop through each question in relationships to the card selected
+            nature[num].textContent = natureArray[num].question;
+            answer.push(natureArray[num].answer)
+
+            //new cards can't be selected until either guess or pass buttons are clicked. They can now enabled
+            // cardSelected = true;
+            guessBtn.disabled = false;
+            passBtn.disabled = false;
+}
+
+
+
+
 
 
 //*************************Nature Questions******************************** */
@@ -114,16 +150,13 @@ let makeHandler = function (num) {
             answer.length > 0 ? answer.pop() : null
             possiblePoints.length > 0 ? possiblePoints.pop() : null
             if (!cardSelected){ // set to false will allow user to select a card
-            card[num].onclick = function () {
-            modal.style.display = "block";
-            };
 
             //captures point value of selected question and parses to number
             possiblePoints.push(nature[num].innerHTML)
 
             //will loop through each question in relationships to the card selected
-            nature[num].textContent = animalArray[num].question;
-            answer.push(animalArray[num].answer)
+            nature[num].textContent = natureArray[num].question;
+            answer.push(natureArray[num].answer)
 
             //new cards can't be selected until either guess or pass buttons are clicked. They can now enabled
             // cardSelected = true;
@@ -137,25 +170,27 @@ for (let i = 0; i < nature.length; i++) {
 }
 
 //*************************Animal Questions******************************** */
-
-//MODAL
-
-card.onclick = function openModal(){
-    modal.style.display = block
-}
-
+let player1Score = document.getElementById("player-1-score")
+let player2Score = document.getElementById("player-2-score")
 
     guessBtn.addEventListener("click", e =>{
             e.preventDefault()
             let pointValue = possiblePoints.toString()
-
+            console.log(answer)
+            console.log(pointValue)
             if(answer.includes(input.value)){
                 Players.addPoints(Number(pointValue))
                 console.log(currentPlayer.points);
+                player1Score.textContent = `Player 1 Score: ${Players.player1.points}`
+                player2Score.textContent = `Player 2 Score: ${Players.player2.points}`
+                guessBtn.disabled = true
+            } else if (!answer.includes(input.value)){
+                Players.subtractPoints(pointValue)
+                Players.changePlayers(currentPlayer)
+                player1Score.textContent = `Player 1 Score: ${Players.player1.points}`;
+                player2Score.textContent = `Player 2 Score: ${Players.player2.points}`;
+                
             }
-        input.value = ""
+       
     })
-    
-    
- 
 
